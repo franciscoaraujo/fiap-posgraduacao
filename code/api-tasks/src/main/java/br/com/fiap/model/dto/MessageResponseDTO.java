a94d3af6cb4ac07e8454e135e45a74de4b3ce8df
franciscoaraujo <chicoaraujo1063@gmail.com>
2022-08-25 22:19:55 -0300

    Autualizando o dto de mensagem

--- code/api-tasks/src/main/java/br/com/fiap/model/dto/MessageResponseDTO.java
+++ code/api-tasks/src/main/java/br/com/fiap/model/dto/MessageResponseDTO.java
@@ -2,11 +2,8 @@ package br.com.fiap.model.dto;
 
 import lombok.Data;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 public class MessageResponseDTO {
     private String message;
-    private List<String> erros = new ArrayList<>();
 }
