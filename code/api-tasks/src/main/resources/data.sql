d105037a6005786405cacdd1e981bf88b8902e0e
franciscoaraujo <chicoaraujo1063@gmail.com>
2022-08-25 23:39:29 -0300

    adicionando configuracao de banco postgres no properties

---
+++ code/api-tasks/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+insert into usertbl("name", "password", created_at,updatade_at)values
+   ('John Contraine', '123456', '2022-08-26', '2022-08-26'),
+   ('StanGetz Machado', '123456', '2022-08-26', '2022-08-26'),
+   ('Deve Weckel sa Silva', '123456', '2022-08-26', '2022-08-26');
+
+
+insert into user_tasktbl("name", "description", created_at, updatade_at, task_finished, id_user)values
+    ('Trabalho', 'Finalizar partitura Countdown', null, '022-08-26', false, 1);
+    ('Estudo', 'Praticar campo harmonico menor', null, '022-08-26', false, 2);
+    ('Estudo', 'Praticar Paradidlle', null, '022-08-26', false, 3);
+    ('Manutencao', 'Trocar as peles da caixa', null, '022-08-26', false, 3);
+
+
